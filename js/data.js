f52116f4906ccb998be688de44ef6feeb0bf17b4
Peter Zhang <petejzh@gmail.com>
2020-12-29 16:08:56 -0500

    pull

---
+++ js/data.js
@@ -0,0 +1,474 @@
+var data = [{
+	"name": "Gwin Zhou",
+	"company": "McKinsey & Company",
+	"position": "Business Analyst",
+	"city": "Palo Alto, CA",
+	"year": "2016",
+	"major": "Business",
+	"type": "Full-Time"
+}, {
+	"name": "Giselle Fang",
+	"company": "Parthenon",
+	"position": "Associate",
+	"city": "San Francisco, CA",
+	"year": "2016",
+	"major": "Business",
+	"type": "Full-Time"
+}, {
+	"name": "Robbie Hollis",
+	"company": "Cornerstone",
+	"position": "Analyst",
+	"city": "San Francisco, CA",
+	"year": "2016",
+	"major": "Business",
+	"type": "Full-Time"
+}, {
+	"name": "Vedika Prakash",
+	"company": "PricewaterhouseCoopers",
+	"position": "Associate Consultant",
+	"city": "San Jose, CA",
+	"year": "2016",
+	"major": "Business",
+	"type": "Full-Time"
+}, {
+	"name": "Joanna Chow",
+	"company": "Accenture",
+	"position": "Management Consulting Analyst",
+	"city": "San Francisco, CA",
+	"year": "2016",
+	"major": "Business",
+	"type": "Full-Time"
+}, {
+	"name": "Savan Patel",
+	"company": "Accenture",
+	"position": "Strategy Analyst",
+	"city": "San Francisco, CA",
+	"year": "2016",
+	"major": "Business",
+	"type": "Full-Time"
+}, {
+	"name": "Stephan Liu",
+	"company": "Uber",
+	"position": "Software Engineering Intern",
+	"city": "New York, NY",
+	"year": "2016",
+	"major": "Engineering",
+	"type": "Internship"
+}, {
+	"name": "Felix Liu",
+	"company": "SurveyMonkey",
+	"position": "Software Engineer",
+	"city": "Palo Alto, CA",
+	"year": "2016",
+	"major": "Engineering",
+	"type": "Full-Time"
+}, {
+	"name": "Joanna Chow",
+	"company": "Kaiser Permanente",
+	"position": "Finance IT",
+	"city": "Pleasanton, CA",
+	"year": "2015",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Megha Bindal",
+	"company": "Deutsche Bank",
+	"position": "Sales and Trading Intern",
+	"city": "New York, NY",
+	"year": "2016",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Krishna Mocherla",
+	"company": "Goldman Sachs",
+	"position": "Investment Banking Analyst Intern",
+	"city": "New York, NY",
+	"year": "2016",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Cherry Wang",
+	"company": "Goldman Sachs",
+	"position": "Investment Banking Analyst Intern",
+	"city": "New York, NY",
+	"year": "2016",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Cherry Wang",
+	"company": "Goldman Sachs",
+	"position": "Investment Banking Analyst Intern",
+	"city": "New York, NY",
+	"year": "2015",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Natalie Rasmus",
+	"company": "Apple",
+	"position": "Finance & Strategy Intern",
+	"city": "Cupertino, CA",
+	"year": "2016",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Margarita Oreta",
+	"company": "Facebook",
+	"position": "Business Intern",
+	"city": "Menlo Park, CA",
+	"year": "2016",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Albert Chiu",
+	"company": "General Electric",
+	"position": "Process and Project Engineering Intern",
+	"city": "Chicago, IL",
+	"year": "2016",
+	"major": "Engineering",
+	"type": "Internship"
+}, {
+	"name": "Ted Xiao",
+	"company": "Amazon",
+	"position": "Software Engineering Intern",
+	"city": "Seattle, WA",
+	"year": "2016",
+	"major": "Engineering",
+	"type": "Internship"
+}, {
+	"name": "Tejas Kannan",
+	"company": "Microsoft",
+	"position": "Software Engineering Intern",
+	"city": "Redmond, WA",
+	"year": "2016",
+	"major": "Engineering",
+	"type": "Internship"
+}, {
+	"name": "Sanchit Shorewala",
+	"company": "Countable .us",
+	"position": "Software Engineering Intern",
+	"city": "San Francisco, CA",
+	"year": "2016",
+	"major": "Engineering",
+	"type": "Internship"
+}, {
+    "name": "Rohit Gupta",
+    "company": "Credit Suisse",
+    "position": "Investment Banking Analyst Intern - Technology",
+    "city": "San Francisco, CA",
+    "year": "2015",
+    "major": "Business",
+    "type": "Internship"
+}, {
+	"name": "Savan Patel",
+	"company": "Altman Vilandrie & Company",
+	"position": "Summer Analyst",
+	"city": "San Francisco, CA",
+	"year": "2015",
+	"major": "Business",
+    "type": "Internship"
+}, {
+    "name": "Giselle Fang",
+    "company": "Altman Vilandrie & Company",
+    "position": "Summer Analyst",
+    "city": "San Francisco, CA",
+    "year": "2015",
+    "major": "Business",
+    "type": "Internship"
+}, {
+	"name": "Sumedh Bhattacharya",
+	"company": "IBM",
+	"position": "Software Development Intern",
+	"city": "San Jose, CA",
+	"year": "2015",
+	"major": "Engineering",
+	"type": "Internship"
+}, {
+	"name": "Lilac Peterson",
+	"company": "US Department of the Treasury",
+	"position": "Intern Shared Services Division",
+	"city": "Washington D.C.",
+	"year": "2015",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Gwin Zhou",
+	"company": "LitiNomics",
+	"position": "Summer Analyst",
+	"city": "Mountain View, CA",
+	"year": "2015",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Amit Patankar",
+	"company": "Google",
+	"position": "Software Engineer",
+	"city": "Mountain View, CA",
+	"year": "2015",
+	"major": "Engineering",
+	"type": "Full-Time"
+}, {
+	"name": "Crystal Jain",
+	"company": "Apple",
+	"position": "Engineering Program Manager",
+	"city": "Cupertino, CA",
+	"year": "2015",
+	"major": "Engineering",
+	"type": "Full-Time"
+}, {
+	"name": "Joanna Chow",
+	"company": "Deloitte",
+	"position": "Advisory Intern",
+	"city": "San Francisco, CA",
+	"year": "2015",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Olivia Gu",
+	"company": "Facebook",
+	"position": "Finance & Strategy Intern",
+	"city": "Menlo Park, CA",
+	"year": "2015",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Alishan Patel",
+	"company": "M & A Capital, LLC",
+	"position": "Investment Banking Summer Analyst",
+	"city": "Los Angeles, CA",
+	"year": "2015",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Megha Bindal",
+	"company": "Oracle",
+	"position": "Corporate Marketing Intern",
+	"city": "Redwood City, CA",
+	"year": "2015",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Tommy Nyugen",
+	"company": "YuMe",
+	"position": "Business Development Intern",
+	"city": "Redwood City, CA",
+	"year": "2015",
+	"major": "Business",
+	"type": "Internship"
+}, {
+    "name": "Robert Hollis",
+    "company": "Wells Fargo Investment Bank",
+    "position": "Public Finance Analyst/Higher Education & Nonprofit",
+    "city": "San Francisco, CA",
+    "year": "2015",
+    "major": "Business",
+    "type": "Full-Time"
+}, {
+    "name": "Michelle Hsu",
+    "company": "ZS Associates",
+    "position": "Business Associate",
+    "city": "San Francisco, CA",
+    "year": "2015",
+    "major": "Business",
+    "type": "Full-Time"
+}, {
+	"name": "Philmon Tanuri",
+	"company": "McKinsey & Company",
+	"position": "Business Analyst Intern",
+	"city": "Jakarta, Indonesia",
+	"year": "2015",
+	"major": "Business",
+	"type": "Internship"
+}, {
+    "name": "Stephan Liu",
+    "company": "Goldman Sachs",
+    "position": "Summer Technology Analyst",
+    "city": "New York City, NY",
+    "year": "2015",
+    "major": "Engineering",
+    "type": "Internship"
+}, {
+	"name": "Jane Sima",
+    "company": "Bain & Company",
+    "position": "Associate Consultant",
+    "city": "San Francisco, CA",
+    "year": "2015",
+    "major": "Business",
+    "type": "Full-Time"
+}, {
+	"name": "Liying Tan",
+    "company": "Deloitte Consulting",
+    "position": "Analyst",
+    "city": "San Francisco, CA",
+    "year": "2015",
+    "major": "Business",
+    "type": "Full-Time"
+}, {
+	"name": "Melissa Zhang",
+    "company": "BlackRock",
+    "position": "Product and Strategy Analyst",
+    "city": "San Francisco, CA",
+    "year": "2015",
+    "major": "Business",
+    "type": "Full-Time"
+},  {
+	"name": "Felix Liu",
+    "company": "SurveyMonkey",
+    "position": "Software Engineering Intern",
+    "city": "Palo Alto, CA",
+    "year": "2015",
+    "major": "Engineering",
+    "type": "Internship"
+}, {
+	"name": "Matthew Wong",
+    "company": "Ebay",
+    "position": "Product Manager Intern",
+    "city": "San Jose, CA",
+    "year": "2015",
+    "major": "Engineering",
+    "type": "Internship"
+}, {
+	"name": "Ted Xiao",
+    "company": "Acupera Software",
+    "position": "Software Engineering Intern",
+    "city": "San Francisco, CA",
+    "year": "2015",
+    "major": "Engineering",
+    "type": "Internship"
+}, {
+    "name": "Ellen Chun",
+    "company": "Bain & Company",
+    "position": "Associate Consultant",
+    "city": "San Francisco, CA",
+    "year": "2014",
+    "major": "Business",
+    "type": "Full-Time"
+}, {
+	"name": "Bingze Gu",
+	"company": "Goldman Sachs",
+	"position": "Summer Analyst",
+	"city": "New York City, NY",
+	"year": "2014",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Olivia Gu",
+	"company": "UBS",
+	"position": "Wealth Management Intern",
+	"city": "San Diego, CA",
+	"year": "2014",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Rohit Gupta",
+	"company": "Deloitte Consulting",
+	"position": "Business Technology Analyst Intern",
+	"city": "San Francisco, CA",
+	"year": "2014",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Robert Hollis",
+	"company": "Kohlberg Kravis Roberts (KKR)",
+	"position": "Intern",
+	"city": "San Francisco, CA",
+	"year": "2014",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Michelle Hsu",
+	"company": "Kiva",
+	"position": "Kiva Zip Intern",
+	"city": "San Francisco, CA",
+	"year": "2014",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Savan Patel",
+	"company": "Box",
+	"position": "User Operations Intern",
+	"city": "Los Altos, CA",
+	"year": "2014",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Jane Sima",
+	"company": "PricewaterhouseCoopers",
+	"position": "Management Consulting Intern",
+	"city": "San Jose, CA",
+	"year": "2014",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Liyang Tan",
+	"company": "Adobe",
+	"position": "Internal Consulting Intern",
+	"city": "San Jose, CA",
+	"year": "2014",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Philmon Tanuri",
+	"company": "Accenture",
+	"position": "Consulting Analyst Intern",
+	"city": "Jakarta, Indonesia",
+	"year": "2014",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Melissa Zhang",
+	"company": "Pacific Investment Management Company (PIMCO)",
+	"position": "Account Analyst Intern",
+	"city": "Newport Beach, CA",
+	"year": "2014",
+	"major": "Business",
+	"type": "Internship"
+}, {
+	"name": "Stephan Liu",
+	"company": "Riverbed Technology",
+	"position": "Engineering Intern",
+	"city": "San Francisco, CA",
+	"year": "2014",
+	"major": "Engineering",
+	"type": "Internship"
+}, {
+	"name": "Amit Patankar",
+	"company": "Apple",
+	"position": "Design Verification Intern",
+	"city": "Cupertino, CA",
+	"year": "2014",
+	"major": "Engineering",
+	"type": "Internship"
+}, {
+	"name": "Matthew Wong",
+	"company": "Splunk",
+	"position": "Software Engineering Intern",
+	"city": "San Francisco, CA",
+	"year": "2014",
+	"major": "Engineering",
+	"type": "Internship"
+}, {
+	"name": "Ted Xiao",
+	"company": "FMC Technologies Schilling Robotics",
+	"position": "Software Engineering Intern",
+	"city": "Davis, CA",
+	"year": "2014",
+	"major": "Engineering",
+	"type": "Internship"
+}, {
+	"name": "Gwin Zhou",
+	"company": "Microsoft",
+	"position": "Software Development Engineer Intern",
+	"city": "Redmond, WA",
+	"year": "2014",
+	"major": "Engineering",
+	"type": "Internship"
+}, {
+	"name": "Felix Liu",
+	"company": "Gap Inc",
+	"position": "Web Software Engineering Intern",
+	"city": "San Francisco, CA",
+	"year": "2014",
+	"major": "Engineering",
+	"type": "Internship"
+}]
+
