be4546ab3a1782f2e20a3951dac0024e8eadd832
Peter Zhang <petejzh@gmail.com>
2021-01-12 19:51:05 -0500

    remove dupes

--- recruiting/match_contacts.py
+++ recruiting/match_contacts.py
@@ -5,15 +5,24 @@
 import csv
 
 # store contacts
+emails = {}
 with open("contacts.csv", 'r', encoding='utf-8-sig') as contactsFile:
-    emails = dict([row for row in csv.reader(contactsFile)])
+    for row in csv.reader(contactsFile):
+        name = row[0]
+        email = row[1]
+        if name in emails:
+            emails[name].append(email)
+        else:
+            emails[name] = [email]
 
 # iterate through emails
 found_emails = []
 with open("canvas_names.txt", 'r') as contacts:
     for name in contacts:
         if name.strip() in emails:
-            found_emails.append(emails[name.strip()])
+            # don't add duplicates (avoid mismatches)
+            if len(emails[name.strip()]) == 1:
+                found_emails.append(emails[name.strip()][0])
 
 # remove dupes
 found_emails = set(found_emails)
