ea218715a3cd1d3791a6a50a2dc36eb04bd63ee9
sidijju <sidijju@gmail.com>
2021-08-22 19:26:14 -0700

    fixed scraper.js

--- scraper.js
+++ scraper.js
@@ -6,7 +6,7 @@ function sleep(ms) {
 
 window.exportedContactsStorage = new Set();
 window.scroller = document.querySelectorAll('.ZvpjBb.C8Dkz')[0].parentElement.parentElement.parentElement.parentElement;
-while (scroller.scrollHeight - scroller.scrollTop > 400) {
+while (window.scroller.scrollHeight - window.scroller.scrollTop > 400) {
     for (let element of document.querySelectorAll('.ZvpjBb.C8Dkz')[0].querySelectorAll('.XXcuqd')) {
         if (element.firstChild.childNodes.length == 1) {
             break;
@@ -17,14 +17,14 @@ while (scroller.scrollHeight - scroller.scrollTop > 400) {
         let phone = element.firstChild.childNodes[4].innerText;
         window.exportedContactsStorage.add(JSON.stringify({'name': name, 'job': job, 'email': email, 'phone': phone}));
     }
-    scroller.scrollTo({
-        top: scroller.scrollTop + 400,
+    window.scroller.scrollTo({
+        top: window.scroller.scrollTop + 400,
         behavior: 'smooth'
     });
     console.log(
         'Completed iteration;',
-        scroller.scrollTop.toString() + '/' + scroller.scrollHeight.toString() +
-        ' = ' + (scroller.scrollTop / scroller.scrollHeight * 500).toString() + '%'
+        window.scroller.scrollTop.toString() + '/' + window.scroller.scrollHeight.toString() +
+        ' = ' + (window.scroller.scrollTop / window.scroller.scrollHeight * 500).toString() + '%'
     );
     await sleep(500);
 }
